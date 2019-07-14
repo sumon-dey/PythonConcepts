69da7dfacc71f2800aa85f2bba11e2d55a4572cd
Sumon <sdtechie22@gmail.com>
2019-07-14 21:20:38 +0530

    Added Strings

---
+++ strings.py
@@ -0,0 +1,19 @@
+#Python Strings
+
+mySampleString="This is a sample string which will be used to demonstrate the string functionality."
+
+#Substring,Slicing,Splitting and other functionalities
+print(mySampleString[2])
+print(mySampleString[5:16])
+sampleStr=" Hello "
+print(sampleStr.strip())
+print(mySampleString.upper())
+print(mySampleString.lower())
+print(mySampleString.replace("s","h"))
+print(mySampleString.split(" "))
+print("**********************************************************")
+
+#Handling User Input
+myInputString=input("How are you feeling?")
+print("You are feeling "+myInputString)
+print("**********************************************************")
