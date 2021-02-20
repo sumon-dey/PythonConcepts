90fea1a283b744b83d2795d9df5fb9d5e45ed637
Sumon <sdtechie22@gmail.com>
2021-02-20 09:36:30 +0530

    Added Pass statements

---
+++ pass.py
@@ -0,0 +1,9 @@
+# Pass
+
+# The "pass" statement does nothing and it can be used when a statement is required syntactically but the program requires no action
+# They can be used inside the loops (for, while), class, functions, conditional statements and everywhere
+
+for number in range(20):
+    # It is not doing anything here but the for loop is still valid
+    pass
+print("**********************************************************")
